48df4e7a954d5c6751532bf3d12678e3baba027e
Lucas Clerisse <lucasclerisse@protonmail.com>
2022-12-30 17:00:03 +0800

    Update naming

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -220,11 +220,11 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Site reliability engineer and entrepreneur.
+            Site Reliability Engineer & Entrepreneur
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Lucas Clerisse, an SRE and entrepreneur based in Hong Kong.
-            I work a Covergo on the SRE team, where we develop technologies that empower insurances to provide better services to their customers.
+            I work at Covergo on the SRE team, where we develop technologies that empower insurances to provide better services.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
